df82fa090a5446d7c483a6ef431b8849c38a7233
Lunarmagpie <bambolambo0@gmail.com>
2022-12-06 02:24:29 -0500

    clean up code

--- day6/solution.hs
+++ day6/solution.hs
@@ -1,9 +1,9 @@
 import Data.List (elemIndex, groupBy, nub)
 
 findUniqueSeries :: Eq a => [a] -> Int -> Int
-findUniqueSeries content seriesLength = seriesLength + matches content
+findUniqueSeries content seriesLength = seriesLength + notMatches content
   where
-    matches = length . takeWhile (not . isMatch . take seriesLength) . iterate (drop 1)
+    notMatches = length . takeWhile (not . isMatch . take seriesLength) . iterate (drop 1)
 
 isMatch :: Eq a => [a] -> Bool
 isMatch arr = nub arr == arr
