28287ce56344a7c30077f20ac47a4e7e603183c3
Lunarmagpie <bambolambo0@gmail.com>
2022-12-08 01:00:54 -0500

    add day 8

---
+++ day8/solution.hs
@@ -0,0 +1,49 @@
+import Data.Char (digitToInt)
+
+map2d = map . map
+
+to2DArray :: String -> [[Int]]
+to2DArray = map2d digitToInt . lines
+
+isVisible :: [[Int]] -> (Int, Int) -> Bool
+isVisible array (x, y) = inner (x, y) (1, 0) || inner (x, y) (-1, 0) || inner (x, y) (0, 1) || inner (x, y) (0, -1)
+  where
+    number = array !! x !! y
+    inner :: (Int, Int) -> (Int, Int) -> Bool
+    inner (x, y) (dx, dy)
+      | x + dx < 0 || x + dx >= length array = True
+      | y + dy < 0 || y + dy >= length (head array) = True
+      | otherwise = array !! (x + dx) !! (y + dy) < number && inner (x + dx, y + dy) (dx, dy)
+
+scenicScore :: [[Int]] -> (Int, Int) -> Int
+scenicScore array (x, y) = inner (x, y) (1, 0) * inner (x, y) (-1, 0) * inner (x, y) (0, 1) * inner (x, y) (0, -1)
+  where
+    number = array !! x !! y
+    inner :: (Int, Int) -> (Int, Int) -> Int
+    inner (x, y) (dx, dy)
+      | x + dx < 0 || x + dx >= length array = 0
+      | y + dy < 0 || y + dy >= length (head array) = 0
+      | array !! (x + dx) !! (y + dy) >= number = 1
+      | otherwise = 1 + inner (x + dx, y + dy) (dx, dy)
+
+coordinateMap :: ([[Int]] -> (Int, Int) -> b) -> [[Int]] -> [[b]]
+coordinateMap f arr = map2d (f arr) $ map (row [0 .. width -1]) [0 .. height -1]
+  where
+    row :: [Int] -> Int -> [(Int, Int)]
+    row [x] n = [(n, x)]
+    row (x : xs) n = (n, x) : row xs n
+    row [] _ = []
+
+    width = length arr
+    height = length (head arr)
+
+main :: IO ()
+main = do
+  input <- readFile "input.txt"
+  let arr = to2DArray input
+
+  let partOne = sum $ map sum $ map2d fromEnum (coordinateMap isVisible arr)
+  let partTwo = maximum $ map maximum (coordinateMap scenicScore arr)
+
+  putStrLn $ "Part one result:" ++ show partOne
+  putStrLn $ "Part two result:" ++ show partTwo
