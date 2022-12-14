995fdd73eb0310e5908a302f1c7df7a4aa4b361b
Lunarmagpie <bambolambo0@gmail.com>
2022-12-14 02:50:45 -0500

    add day 14

--- day13/solution.hs
+++ day13/solution.hs
@@ -1,28 +1,37 @@
 import Data.Char (digitToInt)
-import Data.List.Split (splitOn)
+import Data.List (isPrefixOf)
+import Data.List.Split (splitOn, startsWith)
 import Debug.Trace (trace)
 
-data Packet = IntPacket Int | PacketArray [Packet] deriving (Show)
+data Packet = IntPacket String | PacketArray [Packet] | Unparsed String deriving (Show)
 
 parsePackets = (map . map) parseArray . packets
   where
     packets = map (take 2) . takeWhile (not . null) . iterate (drop 3) . lines
 
     parseArray :: String -> Packet
-    parseArray arr
-      | null arr = PacketArray []
-      | '[' `notElem` arr = IntPacket $ digitToInt (head arr)
-      | otherwise = PacketArray $ map parseArray elems ++ parsedOther
-      where
-        elems = (splitOn "," . takeWhile (/= '[') . init . tail) arr
-        other = (dropWhile (/= '[') . init . tail) arr
+    parseArray packet
+      | packet == "[]" = PacketArray []
+      | "[" `isPrefixOf` packet = (parseArray . init . tail) packet
+      -- Must be a list of numbers
+      | otherwise = (PacketArray . map (IntPacket) . splitOn ",") packet
 
-        parsedOther = case other of
-          "" -> []
-          t -> [parseArray (trace ("\n" ++ show t) t)]
+
+-- takeUntilLevel :: String -> String
+-- takeUntilLevel s = inner s 0
+--   where
+--     inner :: String -> Int -> String
+--     inner ('[' : xs) 0 = ['[']
+--     inner (']' : xs) 0 = [']']
+--     inner ('[' : xs) a = '[' : inner xs (a + 1)
+--     inner (']' : xs) a = ']' : inner xs (a - 1)
+--     inner (x : xs) a = x : inner xs a
+--     inner [] _ = []
 
 main :: IO ()
 main = do
   content <- readFile "input.txt"
 
   print $ parsePackets content
+
+  -- print $ (read "[1,2,3,4]" :: [Int])
---
+++ day14/solution.hs
@@ -0,0 +1,66 @@
+import Data.List (sortOn)
+import Data.List.Split (splitOn)
+import qualified Data.Map as M
+import Data.Maybe (isJust)
+import GHC.Float (float2Int)
+
+type Line = ((Int, Int), (Int, Int))
+
+type Grid = M.Map (Int, Int) Char
+
+toLines :: String -> [Line]
+toLines = map (toLine . map (map read . splitOn ",") . take 2) . init . takeWhile (not . null) . iterate (drop 1) . splitOn " -> "
+  where
+    toLine [[x1, y1], [x2, y2]] = ((x1, y1), (x2, y2))
+
+toPoints :: String -> [(Int, Int)]
+toPoints = map (toPoint . splitOn ",") . splitOn " -> "
+  where
+    toPoint [x1, y1] = (read x1, read y1)
+
+repeatList :: [a] -> [a]
+repeatList = concat . repeat
+
+getMap :: [[Line]] -> Grid
+getMap = foldl (foldl insertLine) M.empty
+  where
+    insertLine :: Grid -> Line -> Grid
+    insertLine grid ((x1, y1), (x2, y2))
+      | y1 < y2 && x1 == x2 = insert [(x1, i) | i <- [y1 .. y2]]
+      | y1 >= y2 && x1 == x2 = insert [(x1, i) | i <- [y2 .. y1]]
+      | x1 < x2 && y1 == y2 = insert [(i, y1) | i <- [x1 .. x2]]
+      | x1 >= x2 && y1 == y2 = insert [(i, y1) | i <- [x2 .. x1]]
+      | otherwise = error "?????"
+      where
+        insert = foldr (`M.insert` '#') grid
+
+dropSand :: Int -> Maybe Grid -> (Int, Int) -> Maybe Grid
+dropSand _ Nothing _ = Nothing
+dropSand max (Just grid) (x, y)
+  | (x, y) `M.member` grid = error "Already contains point"
+  | y > max = Nothing
+  | not ((x, y + 1) `M.member` grid) = dropSand max (Just grid) (x, y + 1)
+  | not ((x - 1, y + 1) `M.member` grid) = dropSand max (Just grid) (x -1, y + 1)
+  | not ((x + 1, y + 1) `M.member` grid) = dropSand max (Just grid) (x + 1, y + 1)
+  | otherwise = Just $ M.insert (x, y) '0' grid
+
+dropSand2 :: Int -> Maybe Grid -> (Int, Int) -> Maybe Grid
+dropSand2 _ Nothing _ = Nothing
+dropSand2 max (Just grid) (x, y)
+  | y >= max - 1 = Just $ M.insert (x, y) '0' grid
+  | not ((x, y + 1) `M.member` grid) = dropSand2 max (Just grid) (x, y + 1)
+  | not ((x - 1, y + 1) `M.member` grid) = dropSand2 max (Just grid) (x -1, y + 1)
+  | not ((x + 1, y + 1) `M.member` grid) = dropSand2 max (Just grid) (x + 1, y + 1)
+  | not ((x, y) `M.member` grid) = Just $ M.insert (x, y) '0' grid
+  | otherwise = Nothing
+
+main = do
+  content <- readFile "input.txt"
+
+  let points = (getMap . map toLines . lines) content
+  let pointsList = (concatMap toPoints . lines) content
+  let max = maximum (map snd pointsList)
+
+  print $ ((+) (-1) . length . takeWhile isJust) $ scanl (dropSand max) (Just points) (repeat (500, 0))
+
+  print $ ((+) (-1) . length . takeWhile isJust) $ scanl (dropSand2 (max + 2)) (Just points) (repeat (500, 0))
